fd0ba2d009fb8a7172d02a85c6f288ee209b27fb
MohitSinghvi <msinghvi16@gmail.com>
2020-10-26 17:37:19 +0530

    Data access object

---
+++ src/com/company/Book.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.Set;
+
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private String description;
+    private Category category;
+    private Set<Review> reviews;
+
+    public Book() {
+    }
+
+    public Set<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(Set<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
